f8910e30f51c28fd933a1bcb0e601ea6aa8a30f0
Atharv Rustagi <atharvrustagi@gmail.com>
2022-03-04 23:39:39 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,15 +2,13 @@
 #include <math.h>
 #include <SFML/Graphics.hpp>
 
+
 sf::Color light_c (sf::Color(255, 255, 0)),
           bg_c    (sf::Color(25, 25, 25));
 sf::RenderWindow win(sf::VideoMode(500, 500), "Ray Casting");
-std::vector<sf::VertexArray> objects;
+std::vector<sf::ConvexShape> objects;
 const sf::Vector2f ws(win.getSize());
-
-sf::Vector2f point_map[12][2] = {
-
-};
+float line_thickness = 3.f;
 
 bool load_cursor(sf::Cursor &cursor)    {
     sf::Image img;
@@ -18,15 +16,22 @@ bool load_cursor(sf::Cursor &cursor)    {
     return cursor.loadFromPixels(img.getPixelsPtr(), img.getSize(), {img.getSize().x / 2, img.getSize().y / 2});
 }
 
-/*
-possible optimisations:
-    make booleans for c and m if they are negative, and checking can be done without divisions
-*/
+sf::Vector2f point_map[] = {
+    {ws.x, 0},
+    {ws.x, ws.y},
+    {0, ws.y},
+    {0, 0}
+};
+
+// possible optimisations:
+//     make booleans for c and m if they are negative, and checking can be done without divisions
 // shape of the shadow to be casted
 sf::ConvexShape shadow_cast(const sf::Vector2f &p1, const sf::Vector2f &p2, const sf::Vector2f &cursor)   {
     sf::ConvexShape shape(6);
     shape.setFillColor(bg_c);
     float d1, d2, m, c;
+    enum {U, R, D, L};
+    short edge[2], e = 0;
     // find intersection of line with the screen borders
     auto intersection = [&]()  {
         sf::Vector2f point;
@@ -36,16 +41,19 @@ sf::ConvexShape shadow_cast(const sf::Vector2f &p1, const sf::Vector2f &p2, cons
             if (d2 < 0 && (-c / m) >= 0)    {
                 point.x = (-c / m);
                 point.y = 0;
+                edge[e++] = U;
             }
             // lower border
             else if (d2 >= 0 && (ws.y - c) / m >= 0)  {
                 point.x = (ws.y - c) / m;
                 point.y = ws.y;
+                edge[e++] = D;
             }
             // left border
             else    {
                 point.x = 0;
                 point.y = c;
+                edge[e++] = L;
             }
         }
         // possible right border
@@ -54,16 +62,19 @@ sf::ConvexShape shadow_cast(const sf::Vector2f &p1, const sf::Vector2f &p2, cons
             if (d2 < 0 && (-c / m) <= ws.x)    {
                 point.x = (-c / m);
                 point.y = 0;
+                edge[e++] = U;
             }
             // lower border
             else if (d2 >= 0 && (ws.y - c) / m <= ws.x)  {
                 point.x = (ws.y - c) / m;
                 point.y = ws.y;
+                edge[e++] = D;
             }
             // right border
             else    {
                 point.x = ws.x;
                 point.y = ws.x * m + c;
+                edge[e++] = R;
             }
         }
         return point;
@@ -86,39 +97,116 @@ sf::ConvexShape shadow_cast(const sf::Vector2f &p1, const sf::Vector2f &p2, cons
     sf::Vector2f insec2 = intersection();
 
     // set the points
-    shape.setPoint(0, insec1);
-    shape.setPoint(1, insec2);
+    shape.setPoint(0, p1);
+    shape.setPoint(1, insec1);
+    shape.setPoint(4, insec2);
+    shape.setPoint(5, p2);
+    
     // calculate points 2 and 3 through border info of insec1 and insec2
-    // 12 possible cases
-    
-    
-    shape.setPoint(4, p2);
-    shape.setPoint(5, p1);
-
-    // corner point
-    
-
+    if (edge[0] != edge[1]) {
+        short e0 = edge[0], e1 = edge[1];
+        if (e0 > e1) std::swap(e0, e1);
+        // adjacent edges, both vertices 2 and 3 will be equal
+        if (e0 + 1 == e1)   {
+            e = (e0 + e1) >> 1;
+            shape.setPoint(2, point_map[e]);
+            shape.setPoint(3, point_map[e]);
+        }
+        // adjacent special case
+        else if (e0 == U && e1 == L)  {
+            shape.setPoint(2, point_map[3]);
+            shape.setPoint(3, point_map[3]);
+        }
+        // opposite edges
+        else    {
+            // find the center of the line
+            float cx = (p1.x + p2.x) / 2, cy = (p1.y + p2.y) / 2;
+            // if left and right edge
+            if (e0 & 1)    {
+                // cast shadow up
+                if (cursor.y > cy) {
+                    shape.setPoint(2, point_map[(edge[0] == 1 ? 0 : 3)]);
+                    shape.setPoint(3, point_map[(edge[1] == 1 ? 0 : 3)]);
+                }
+                // cast shadow down
+                else    {
+                    shape.setPoint(2, point_map[(edge[0] == 1 ? 1 : 2)]);
+                    shape.setPoint(3, point_map[(edge[1] == 1 ? 1 : 2)]);
+                }
+            }
+            // up and down edge
+            else    {
+                // cast shadow left
+                if (cursor.x > cx) {
+                    shape.setPoint(2, point_map[(edge[0] ? 2 : 3)]);
+                    shape.setPoint(3, point_map[(edge[1] ? 2 : 3)]);
+                }
+                // cast shadow right
+                else    {
+                    shape.setPoint(2, point_map[(edge[0] ? 1 : 0)]);
+                    shape.setPoint(3, point_map[(edge[1] ? 1 : 0)]);
+                }
+            }
+        }
+    }
+    // on the same edge
+    else    {
+        shape.setPoint(2, shape.getPoint(4));
+        shape.setPoint(3, shape.getPoint(1));
+    }
     return shape;
 }
 
-
-
 // ray casting
 void ray_cast(sf::RenderWindow &win, const sf::Vector2f &cursor_position)    {
+    for (sf::ConvexShape &obj: objects)
+        win.draw(obj);
     // initially everything is lit up
     win.clear(light_c);
     // for every line in obstacles, draw shadows
-    for (sf::VertexArray &obj: objects) {
-        int num_vertices = obj.getVertexCount();
+    for (sf::ConvexShape &obj: objects) {
+        int num_vertices = obj.getPointCount();
         // if only 2 vertices are there, only 1 iteration is required
         if (num_vertices < 3)   {
-            sf::ConvexShape shadow = shadow_cast(obj[0].position, obj[1].position, cursor_position);
+            sf::ConvexShape shadow = shadow_cast(obj.getPoint(0), obj.getPoint(1), cursor_position);
+            win.draw(shadow);
+        }
+        else    {
+            // cast shadows for all adjacent vertex lines
+            for (int i=1; i<num_vertices; ++i)  {
+                sf::ConvexShape shadow = shadow_cast(obj.getPoint(i-1), obj.getPoint(i), cursor_position);
+                win.draw(shadow);
+            }
+            sf::ConvexShape shadow = shadow_cast(obj.getPoint(0), obj.getPoint(num_vertices-1), cursor_position);
             win.draw(shadow);
         }
     }
-    for (sf::VertexArray &obj: objects)
-        win.draw(obj);
 }
+
+void add_object()   {
+    objects.push_back(sf::ConvexShape(4));
+    objects.back().setPoint(0, {100, 100});
+    objects.back().setPoint(1, {200, 100});
+    objects.back().setPoint(2, {200, 102});
+    objects.back().setPoint(3, {100, 102});
+    objects.back().setFillColor(sf::Color::Black);
+    objects.back().setOutlineColor(sf::Color::White);    
+
+    // objects.push_back(sf::VertexArray(sf::Lines, 2));
+    // objects.back()[0].position = {200, 200};
+    // objects.back()[1].position = {300, 300};
+    // objects.back()[0].color = sf::Color::White;
+    // objects.back()[1].color = sf::Color::White;
+
+    objects.push_back(sf::ConvexShape(4));
+    objects.back().setPoint(0, {200, 200});
+    objects.back().setPoint(1, {300, 200});
+    objects.back().setPoint(2, {300, 300});
+    objects.back().setPoint(3, {200, 300});
+    objects.back().setFillColor(sf::Color::Black);
+    objects.back().setOutlineColor(sf::Color::White);    
+}
+
 
 int main()  {
     // printf("%f %f\n", ws.x, ws.y);
@@ -131,12 +219,7 @@ int main()  {
     sf::Cursor cursor;
     if (load_cursor(cursor))
         win.setMouseCursor(cursor);
-    objects.push_back(sf::VertexArray(sf::Lines, 2));
-    objects[0][0].position = {100, 100};
-    objects[0][1].position = {200, 100};
-    objects[0][0].color = sf::Color::Black;
-    objects[0][1].color = sf::Color::Black;
-    // sf::PrimitiveType p = objects[0].getPrimitiveType();
+    add_object();
     // main loop
     while (win.isOpen())    {
         while (win.pollEvent(event))
@@ -150,125 +233,3 @@ int main()  {
     }
     return 0;
 }
-
-
-/*
-class ParticleSystem : public sf::Drawable, public sf::Transformable
-{
-public:
-
-    ParticleSystem(unsigned int count) :
-    m_particles(count),
-    m_vertices(sf::Points, count),
-    m_lifetime(sf::seconds(3)),
-    m_emitter(0, 0)
-    {
-    }
-
-    void setEmitter(sf::Vector2f position)
-    {
-        m_emitter = position;
-    }
-
-    void update(sf::Time elapsed)
-    {
-        for (std::size_t i = 0; i < m_particles.size(); ++i)
-        {
-            // update the particle lifetime
-            Particle& p = m_particles[i];
-            p.lifetime -= elapsed;
-
-            // if the particle is dead, respawn it
-            if (p.lifetime <= sf::Time::Zero)
-                resetParticle(i);
-
-            // update the position of the corresponding vertex
-            m_vertices[i].position += p.velocity * elapsed.asSeconds();
-
-            // update the alpha (transparency) of the particle according to its lifetime
-            float ratio = p.lifetime.asSeconds() / m_lifetime.asSeconds();
-            m_vertices[i].color.a = static_cast<sf::Uint8>(ratio * 255);
-        }
-    }
-
-private:
-
-    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const
-    {
-        // apply the transform
-        states.transform *= getTransform();
-
-        // our particles don't use a texture
-        states.texture = NULL;
-
-        // draw the vertex array
-        target.draw(m_vertices, states);
-    }
-
-private:
-
-    struct Particle
-    {
-        sf::Vector2f velocity;
-        sf::Time lifetime;
-    };
-
-    void resetParticle(std::size_t index)
-    {
-        // give a random velocity and lifetime to the particle
-        float angle = (std::rand() % 360) * 3.14f / 180.f;
-        float speed = (std::rand() % 50) + 50.f;
-        m_particles[index].velocity = sf::Vector2f(std::cos(angle) * speed, std::sin(angle) * speed);
-        m_particles[index].lifetime = sf::milliseconds((std::rand() % 2000) + 1000);
-
-        // reset the position of the corresponding vertex
-        m_vertices[index].position = m_emitter;
-    }
-
-    std::vector<Particle> m_particles;
-    sf::VertexArray m_vertices;
-    sf::Time m_lifetime;
-    sf::Vector2f m_emitter;
-};
-And a little demo that uses it:
-
-int main()
-{
-    // create the window
-    sf::RenderWindow window(sf::VideoMode(512, 256), "Particles");
-
-    // create the particle system
-    ParticleSystem particles(1000);
-
-    // create a clock to track the elapsed time
-    sf::Clock clock;
-
-    // run the main loop
-    while (window.isOpen())
-    {
-        // handle events
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if(event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        // make the particle system emitter follow the mouse
-        sf::Vector2i mouse = sf::Mouse::getPosition(window);
-        particles.setEmitter(window.mapPixelToCoords(mouse));
-
-        // update it
-        sf::Time elapsed = clock.restart();
-        particles.update(elapsed);
-
-        // draw it
-        window.clear();
-        window.draw(particles);
-        window.display();
-    }
-
-    return 0;
-}
-
-*/
